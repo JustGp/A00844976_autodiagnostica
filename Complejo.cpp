74283d88ff1287ef8e0743abf051c6ac32776192
Gp <157935064+JustGp@users.noreply.github.com>
2026-06-30 17:31:57 +0000

    Update

--- Complejo.cpp
+++ Complejo.cpp
@@ -1,17 +1,46 @@
 #include "Complejo.hpp"
-using namespace std;
-
+#include <iostream>
 Complejo::Complejo(double ValR, double ValI):real(ValR),imaginario(ValI){
 }
 
-void Complejo::agregar(double ValR, double ValI){
-    real = real + ValR;
-    imaginario = imaginario + ValI;
+double Complejo::getReal() const{
+    return real;
 
 }
 
-double Complejo::conjugado(){
-    
+double Complejo::getImaginario() const{
+    return imaginario;
+}
+
+
+
+void Complejo::agregar(Complejo& numero){
+    real = real + numero.getReal();
+    imaginario = imaginario + numero.getImaginario();
+
+}
+/*
+Complejo Complejo::conjugado(Complejo valor){
+    return 0;
+}
+
+
+Complejo Complejo::suma(Complejo numero){
+    return 0;
+}
+
+Complejo Complejo::multiplicacion(Complejo numero){
+    return 0;
+}
+
+Complejo Complejo::multiEscalar(double escalar){
+    return 0;
+
+}
+*/
+void Complejo::imprime() {
+    std::cout<< "real" << real << " imaginario " << imaginario << std::endl;
+
 }
 
 
