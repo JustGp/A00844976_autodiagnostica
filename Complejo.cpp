9224a62800d7906111f57799e860c04b9fb6ad9e
Gp <157935064+JustGp@users.noreply.github.com>
2026-06-30 03:43:24 +0000

    Dolor y sufrimiento 1

--- Complejo.cpp
+++ Complejo.cpp
@@ -1,3 +1,23 @@
 #include "Complejo.hpp"
+using namespace std;
 
+Complejo::Complejo(double ValR, double ValI):real(ValR),imaginario(ValI){
+}
+
+void Complejo::agregar(double ValR, double ValI){
+    real = real + ValR;
+    imaginario = imaginario + ValI;
+
+}
+
+double Complejo::conjugado(){
+    
+}
+
+
+int main(){
+    Complejo();
+    Complejo{2,3};
+    return 0;
+}
 
